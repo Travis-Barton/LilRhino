51fdcea44689f4586e14b2295367ca96f68d6e0f
Travis <tbarton@csumb.edu>
2019-02-03 22:43:48 -0800

    Update

--- R/Personal_Functions.R
+++ R/Personal_Functions.R
@@ -212,4 +212,66 @@ Feed_Reduction = function(X, Y, X_test, val_split = .1, nodes = NULL, epochs = 1
   return(list(train = final_train, test = final_test))
 }
 
+## 0) Pretreatment
 
+Pretreatment = function(title_vec){
+  titles = as.character(title_vec) %>%
+    lapply(gsub, pattern = "[^[:alnum:][:space:]]",replacement = "") %>%
+    lapply(stemDocument) %>%
+    lapply(tolower)
+  return(titles)
+}
+
+## 1) Stopword maker
+
+
+StopWordMaker = function(titles, cutoff = 20){
+  test = unlist(lapply(as.vector(titles), strsplit, split = ' ', fixed = TRUE))
+  stopwords = test %>%
+    table() %>%
+    sort(decreasing = TRUE) %>%
+    head(cutoff) %>%
+    names()
+  return(stopwords)
+}
+
+
+## 2) Auto-creating word matrix
+
+
+Embedding_Matrix = function(words, vocab_min, stopwords, skip_gram, vector_size){
+  it   = itoken(unlist(words), tolower, word_tokenizer)
+  vocab = create_vocabulary(it, stopwords = stopwords)
+  vocab <- prune_vocabulary(vocab, term_count_min = vocab_min)
+  # Use our filtered vocabulary
+  vectorizer <- vocab_vectorizer(vocab)
+  # use window of 5 for context words
+  tcm <- create_tcm(it, vectorizer, skip_grams_window = skip_gram)
+  glove = GlobalVectors$new(word_vectors_size = vector_size, vocabulary = vocab, x_max = 5)
+  glove$fit_transform(tcm, n_iter = 20)
+  return(glove$components)
+}
+colnames(temp)
+
+## 3) Sentence Converter
+
+###### 3a) word puller
+Vector_puller = function(word, emb_matrix){
+  if(word %in% rownames(emb_matrix)){
+    return(emb_matrix[word,])
+  }
+  else{
+    return(rep(0, ncol(emb_matrix)))
+  }
+}
+
+Sentence_Vector = function(sentence, emb_matrix, stopwords){
+  words = strsplit(sentence, " ", fixed = TRUE)[[1]]
+  #  for(i in 1:4){
+  #    Vector_puller(word[i], emb_matrix)
+  #
+  #  }
+  vec = lapply(words, Vector_puller, emb_matrix)
+  df <- data.frame(matrix(unlist(vec), nrow=ncol(emb_matrix), byrow=F))
+  return(rowSums(df)/length(unique(colSums(df))))
+}
